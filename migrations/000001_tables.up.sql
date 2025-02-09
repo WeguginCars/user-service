94fe40bd1512a9a4dcc81952793645d3885d0a6c
nurmuhammad701 <nurmuhammadmel@gmail.com>
2025-02-09 20:47:32 +0500

    all

--- migrations/000001_tables.up.sql
+++ migrations/000001_tables.up.sql
@@ -2,8 +2,8 @@ CREATE TYPE roles AS ENUM ('admin','user');
 
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name VARCHAR(50) NOT NULL,
-    surname VARCHAR(50) UNIQUE NOT NULL,
+    name VARCHAR(50),
+    surname VARCHAR(50),
     email VARCHAR(100) UNIQUE NOT NULL,
     birth_date DATE,
     gender VARCHAR(10) CHECK (gender IN ('male', 'female', 'other')),
